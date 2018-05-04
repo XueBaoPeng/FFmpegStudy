69cbf5e25c588d24b55fcf1e778a1c113ac51917
xuebaopeng <13519297683@163.com>
2018-05-04 20:15:50 +0800

    1.测试编译通过ffmpeg架包

--- app/src/main/java/com/xbp/ffmpeg/ffmpegstudy/MainActivity.java
+++ app/src/main/java/com/xbp/ffmpeg/ffmpegstudy/MainActivity.java
@@ -1,11 +1,13 @@
 package com.xbp.ffmpeg.ffmpegstudy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewDataBinding dataBinding;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -14,11 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        dataBinding=DataBindingUtil.setContentView(this,R.layout.activity_main);
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     /**
